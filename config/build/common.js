12cac66167f3fca2063382ebc7a0878ea866b97e
Egor Melnichenko <dinolike@yandex-team.ru>
2023-08-07 15:16:43 +0300

    add dynamic require

--- config/build/common.js
+++ config/build/common.js
@@ -1,13 +1,5 @@
 const path = require("path");
 
-const miniCssExtractPlugin = require("../plugins/miniCssExtractPlugin");
-const htmlWebpackPlugin = require("../plugins/htmlWebpackPlugin");
-
-const tsx = require("../rules/tsx");
-const scss = require("../rules/scss");
-const images = require("../rules/images");
-const fonts = require("../rules/fonts");
-
 module.exports = {
   entry: {
     index: './src/js/index.ts',
@@ -20,15 +12,15 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    miniCssExtractPlugin,
-    htmlWebpackPlugin,
+    require("../plugins/miniCssExtractPlugin"),
+    require("../plugins/htmlWebpackPlugin"),
   ],
   module: {
     rules: [
-      tsx,
-      scss,
-      images,
-      fonts,
+      require("../rules/tsx"),
+      require("../rules/scss"),
+      require("../rules/images"),
+      require("../rules/fonts"),
     ]
   },
   optimization: {
